d919c1a368d0972a28825d0b7fa73a73d124653f
Anderson Almeida <andersonpa10pa@gmail.com>
2023-12-02 10:42:01 -0300

    Criação do componente Button

---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+   width: 100%;
+   padding: 1.6rem 0;
+   
+   background-color: ${({theme}) => theme.colors.pinkLight};
+   color: ${({theme}) => theme.colors.backgroundTags};
+
+   transition: all .5s;
+
+   &:hover {
+      background-color: ${({theme}) => theme.colors.backgroundTags};
+      color: ${({theme}) => theme.colors.pinkLight};
+   }
+
+   &:disabled {
+      opacity: 0.5;
+   }
+`;
